8d587a48d8f111b9749fa0474d6f7f63ed5e5db9
Salim Daif <salim.daif@gmail.com>
2017-02-14 13:25:03 -0200

    TO DO LIST

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name: "Work Out", description: "5000 daily push-ups", status: true)
+Task.create(name: "Eat healty", description: "15000 daily calories", status: false)
+Task.create(name: "Sleep early", description: "12h of sleep/day", status: true)
