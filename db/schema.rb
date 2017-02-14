a73c94aba65f39e493d368ad6882840a135b2b16
Salim Daif <salim.daif@gmail.com>
2017-02-14 11:09:33 -0200

    AddDescroption

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214130113) do
+ActiveRecord::Schema.define(version: 20170214130819) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.boolean  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
 end
